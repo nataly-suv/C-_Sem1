2aba3b1ee665369ac1485f5b9d9464b76c20b90f
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 12:51:34 +0300

    Первая задача. Поиск квадрата числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// первая задача на 1-ом семинаре. Поиск квадрата числа
+Console.WriteLine("Введите число");
+String? innumber = Console.ReadLine();
+if (innumber != null)
+{
+    int number = int.Parse (innumber);
+    int outNumber = number*number;
+    Console.Write("ответ: ");
+    Console.Write(outNumber);
+}
