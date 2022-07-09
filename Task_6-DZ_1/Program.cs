f99b06ef9ac607b143e5e6f8b377157441f5b2b1
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 19:23:48 +0300

    Домашнее задание 1. Поиск максимального

---
+++ Task_6-DZ_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Домашнее задание 1. Поиск максимального
+Console.WriteLine("Введите первое число");
+string? inNumber1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? inNumber2 = Console.ReadLine();
+if (inNumber1 != null && inNumber2 != null)
+{
+    int number1 = int.Parse(inNumber1);
+    int number2 = int.Parse(inNumber2);
+    if (number1 > number2)
+    {
+        Console.Write("Максимальное число - ");
+        Console.Write(number1);
+    }
+    else
+    {
+        Console.Write("Максимальное число - ");
+        Console.Write(number2);
+    }
+}
