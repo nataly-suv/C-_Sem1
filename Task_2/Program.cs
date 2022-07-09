3ceb68758cd8613a3da79964d2dc61896546fd78
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 16:39:15 +0300

    Вторая задача. Яв-ли первое число квадратом второго

---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// вторая задача на 1-ом семинаре. Яв-ля ли первое число квадратом второго
+Console.WriteLine("Введите первое число");
+String? inNumber1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+String? inNumber2 = Console.ReadLine();
+if (inNumber1 != null && inNumber2 != null)
+{
+    int number1 = int.Parse(inNumber1);
+    int number2 = int.Parse(inNumber2);
+    // if (number1 == number2 * number2)
+    // {
+    //     Console.Write("Yes");
+    // }
+    // else
+    // {
+    //     Console.Write("No");
+    // }
+    if (Math.Sqrt(number1) == number2)
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
+}
