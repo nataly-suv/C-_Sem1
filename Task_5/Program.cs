3e38d6ec5ddff5e3220371b5b412d3f384eb172c
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 18:30:45 +0300

    Пятая задача. Вывести последний символ

---
+++ Task_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// пятая задача. Показывает последюю цифру 3-х значного числа
+Console.WriteLine("Введите 3-х значное число");
+// string? inNumber = Console.ReadLine();
+// if (inNumber != null)
+// {
+//     int number = int.Parse(inNumber);
+//     int outNumber = number%10;
+//     Console.WriteLine(outNumber);
+
+// }
+
+string? inNumber = Console.ReadLine();
+if (inNumber != null)
+{
+    char [] M = inNumber.ToCharArray();
+    Console.WriteLine(M[2]);
+}
