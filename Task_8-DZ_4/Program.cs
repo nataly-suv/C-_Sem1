7542ed9107e95483933f12caa516350a0f1e5eca
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 20:17:24 +0300

    Домашнее задание 4. Вывод четных чисел

---
+++ Task_8-DZ_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Домашее задание 4. Вывод четных чисел
+Console.WriteLine("Введите число");
+string? inNumber = Console.ReadLine();
+if (inNumber != null)
+{
+    int number = int.Parse(inNumber);
+    string line = "";
+    int countNumber = 2;
+    if (number > 1)
+    {
+        while (countNumber < number)
+        {
+            line = line + countNumber + ", ";
+            countNumber = countNumber + 2;
+        }
+        if (number % 2 == 0)
+        {
+            line = line + number;
+        }
+        Console.WriteLine(line);
+    }
+    else
+    {
+        Console.WriteLine("нет значений, удовлетворяющих условию");
+    }
+}
