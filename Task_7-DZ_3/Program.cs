8a359cd5443300c9a539820a81c54dd8dc28b264
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 19:47:05 +0300

    Домашнее задание 3. Проверка числа на четность

---
+++ Task_7-DZ_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Домашнее задание 3. Проверка числа начетность
+Console.WriteLine("Введите число");
+string? inNumber = Console.ReadLine();
+if (inNumber != null)
+{
+    int number = int.Parse(inNumber);
+    if (number%2 == 0)
+    {
+        Console.WriteLine("число четное");
+    }
+    else{
+        Console.WriteLine("число нечетное");
+    }
+}
