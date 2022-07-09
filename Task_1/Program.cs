ab6fa5f08f0b52d370177029e6330e7af2c417e5
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-09 12:56:00 +0300

    Первая задача. Поиск квадрата числа. Вариант 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,8 +3,9 @@ Console.WriteLine("Введите число");
 String? innumber = Console.ReadLine();
 if (innumber != null)
 {
-    int number = int.Parse (innumber);
-    int outNumber = number*number;
+    int number = int.Parse(innumber);
+    //int outNumber = number*number;
+    int outNumber = (int)Math.Pow(number, 2);
     Console.Write("ответ: ");
     Console.Write(outNumber);
 }
