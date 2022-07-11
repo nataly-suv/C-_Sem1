f3694b81db4c2e2edd6c9c2fc6c56992b2ad4988
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-11 18:17:27 +0300

    Задание 4. Вывод четных чисел от 1 до n

--- Task_8-DZ_4/Program.cs
+++ Task_8-DZ_4/Program.cs
@@ -1,23 +1,27 @@
-﻿// Домашее задание 4. Вывод четных чисел
+﻿// Домашее задание 4. Вывод четных чисел от 1 до N
 Console.WriteLine("Введите число");
-string? inNumber = Console.ReadLine();
+string? inNumber = Console.ReadLine(); 
 if (inNumber != null)
 {
     int number = int.Parse(inNumber);
-    string line = "";
-    int countNumber = 2;
+    string line = "";              // задаем пустую строку
+    int countNumber = 2;  // присваиваем счетчику значение 2. Т.к. выводим четные числа от 1, т.е. начем сразу с 2
     if (number > 1)
     {
-        while (countNumber < number)
+        while (countNumber < number)    // проверка: если наш счетчик меньше введенного чила, то остаемся в цикле
         {
-            line = line + countNumber + ", ";
-            countNumber = countNumber + 2;
+            line = line + countNumber + ", ";  // добавляем в строку значение счетчика
+            countNumber = countNumber + 2;     // увеличиваем счетчик на 2, т.к. нам нужны тольк очетные числа
         }
-        if (number % 2 == 0)
+        if (number % 2 == 0)   // проверяем само введенное число на четность, чтобы добавить его в строку
         {
             line = line + number;
         }
-        Console.WriteLine(line);
+        char[] del = {',', ' '};     
+        // создали массив del с типом char, в который вложили ',' и пробел (эти символы будем удалять с конца)
+        string lineItog = line.TrimEnd(del);  
+        // создали новую строку lineItog, обрадились к методу TrimEnd, чтобы с троке line удалить "," и пробел
+        Console.WriteLine(lineItog);
     }
     else
     {
